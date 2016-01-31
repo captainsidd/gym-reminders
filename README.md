364b1076909c52a5c5ce1da6144edc891d940e7f
Sid Nutulapati <sidgonuts@gmail.com>
2016-01-30 20:00:55 -0500

    fin.

---
+++ README.md
@@ -0,0 +1,4 @@
+# gym-reminders
+===============
+
+A server that sends an SMS alert to me reminding me to go to the gym.
---
+++ index.js
@@ -0,0 +1,25 @@
+var twilio = require('twilio');
+
+var ACCOUNTSID = 'AC336b5f57436f64eedc1b44237059804e';
+var AUTHTOKEN = '3b8322ecf21c30e363f9becb91da74ea';
+
+var client = twilio(ACCOUNTSID, AUTHTOKEN);
+var cronJob = require('cron').CronJob;
+
+
+var SMSdata = {
+  to:'+15854891908',
+  from: '+15852854240',
+  body: 'DO YOU WANNA BE AWESOME? HIT THE GYM, FOOL.'
+};
+
+var textJob = new cronJob( '45 5 * * *', function(){
+  client.sendMessage(SMSdata, function(err, responseData) {
+      if (err) {
+        console.log(err);
+      }
+      if (!err){
+        console.log('Message sent!');
+      }
+  });
+},  null, true);
